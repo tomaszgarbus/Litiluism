a77a3ac4d3baef119443d9e74f7d247231389103
Tomasz Garbus <tomasz.garbus1@gmail.com>
2024-01-22 17:03:37 +0100

    Move forward with statistics

--- app/src/main/java/com/tgarbus/litiluism/ui/StatisticsScreen.kt
+++ app/src/main/java/com/tgarbus/litiluism/ui/StatisticsScreen.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,8 +17,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.tgarbus.litiluism.ui.Fonts.Companion.sarabunFontFamily
 import com.tgarbus.litiluism.ui.reusables.FullScreenPaddedColumn
 import com.tgarbus.litiluism.ui.reusables.Header
 import com.tgarbus.litiluism.ui.reusables.SemiCircularProgressBar
@@ -29,9 +35,34 @@ fun Tile(modifier: Modifier, content: @Composable () -> Unit) {
             )
             .clip(RoundedCornerShape(size = 21.dp))
             .background(Color.White)
+            .padding(10.dp)
     )
     {
         content()
+    }
+}
+
+@Composable
+fun TileWithSemiCircularProgressBar(
+    progressPercentage: Int,
+    description: String,
+    tileModifier: Modifier
+) {
+    Tile(modifier = tileModifier)
+    {
+        Column(Modifier.fillMaxSize()) {
+            Box(Modifier.weight(2f)) {
+                SemiCircularProgressBar(progressPercentage,
+                    canvasModifier = Modifier.fillMaxSize())
+            }
+            Box(Modifier.weight(1f)) {
+                Text(
+                    description,
+                    fontFamily = sarabunFontFamily,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
 
@@ -42,20 +73,16 @@ fun StatisticsScreen(navController: NavController) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            Tile(modifier = Modifier.weight(1f).aspectRatio(1f))
-            {
-                Column() {
-                    SemiCircularProgressBar(69)
-                    Text("lessons completed")
-                }
-            }
-            Tile(modifier = Modifier.weight(1f).aspectRatio(1f))
-            {
-                Column() {
-                    SemiCircularProgressBar(30)
-                    Text("exercises completed")
-                }
-            }
+            TileWithSemiCircularProgressBar(
+                progressPercentage = 69,
+                description = "lessons completed",
+                tileModifier = Modifier.weight(1f).aspectRatio(1f)
+            )
+            TileWithSemiCircularProgressBar(
+                progressPercentage = 20,
+                description = "exercises completed",
+                tileModifier = Modifier.weight(1f).aspectRatio(1f)
+            )
         }
         StatisticsLineChart("Progress", values = listOf())
     }
