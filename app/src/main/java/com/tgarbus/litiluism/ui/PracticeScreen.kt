7830e3847382493a6e1a9477964b98c64abd4c35
Tomasz Garbus <tomasz.garbus1@gmail.com>
2024-02-09 21:49:51 +0100

    Update PracticeScreen.kt

--- app/src/main/java/com/tgarbus/litiluism/ui/PracticeScreen.kt
+++ app/src/main/java/com/tgarbus/litiluism/ui/PracticeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -51,8 +52,11 @@ fun DestinationType.toDisplayableString(): String {
 @Composable
 fun PracticeScreen(navController: NavController) {
     var runeRowDialogDestination = remember { mutableStateOf<DestinationType?>(null) }
+    val scrollState = rememberScrollState()
     Box(modifier = Modifier.fillMaxSize()) {
-        FullScreenPaddedColumn {
+        FullScreenPaddedColumn(
+            unownedScrollState = scrollState
+        ) {
             Header("Practice")
             val balloonsQueue = BalloonsQueue()
             IntroTooltip(
@@ -116,6 +120,7 @@ fun PracticeScreen(navController: NavController) {
                 id = "transliteration",
                 text = "Practice transliteration of full inscriptions!",
                 queue = balloonsQueue,
+                scrollState = scrollState,
                 modifier = Modifier.fillMaxWidth(),
             ) {
                 Row(modifier = Modifier.fillMaxWidth()) {
