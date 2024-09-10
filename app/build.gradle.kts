8efa60fc94fed83b85e754f2eea56dd008d4af74
Tomasz Garbus <tomasz.garbus1@gmail.com>
2024-09-10 18:10:31 +0200

    September update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tgarbus.litiluism"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.06"
+        versionCode = 8
+        versionName = "1.07"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
