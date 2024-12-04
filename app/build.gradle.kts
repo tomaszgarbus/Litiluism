ff26f4f69c7d4bc0b3898e0c7ac0d4b7691c76c9
Tomek Garbus <tomek.garbus@wolt.com>
2024-12-04 23:53:09 +0100

    Next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tgarbus.litiluism"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 13
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
